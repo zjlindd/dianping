3fabc9e248580e6f14f4e77b1efd43179f945463
zjlindd <3190547371@qq.com>
2018-01-03 13:18:34 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,8 @@ let OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
     entry: path.resolve(__dirname, 'app/index.jsx'),
     output: {
+        path: __dirname + "/build",
         filename: "bundle.js"
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
     },
     module: {
         rules: [
@@ -20,22 +18,29 @@ module.exports = {
                 test: /(\.jsx|\.js)$/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "env", "react"
-                        ]
-                    }
                 },
                 exclude: /node_modules/
             },
-            {test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less'},
-            {test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss'},
-            {test: /\.(png|gif|jpg|jpeg|bmp)$/i, loader: 'url-loader?limit=5000'},  // 限制大小5kb
-            {test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader: 'url-loader?limit=5000'} // 限制大小小于5k
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use:[ 'style-loader','css-loader','postcss-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
+                use: ["url-loader?limit=5000"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i,
+                use: ["url-loader?limit=5000"],
+                exclude: /node_modules/
+            },
+
         ]
     },
     plugins: [
-        // html 模板插件
+        // html模板插件
         new HtmlWebpackPlugin({
             template: __dirname + '/app/index.tmpl.html'
         }),
@@ -48,14 +53,10 @@ module.exports = {
             url: 'http://localhost:8080'
         }),
 
-        // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
-        new webpack.DefinePlugin({
-            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
-        })
+
     ],
 	devtool: 'eval-source-map',
     devServer: {
-        colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转，在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true, //实时刷新
         hot: true  // 使用热加载插件 HotModuleReplacementPlugin
