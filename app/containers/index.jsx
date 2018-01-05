ddaef34cc01012f4c0446c0aba5afc7081b82b7e
zjlindd <3190547371@qq.com>
2018-01-05 15:27:54 +0800

    新框架webpack1.1

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
-
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import LocalStore from '../util/localStore'
 import { CITYNAME } from '../config/localStoreKey'
-
+import * as userInfoActionsFromOtherFile from '../actions/userinfo' 
 
 class App extends React.Component {
     constructor(props, context) {
@@ -26,7 +26,35 @@ class App extends React.Component {
         )
     }
     componentDidMount() {
+        // 获取位置信息
+        let cityName = LocalStore.getItem(CITYNAME)
+        if (cityName == null) {
+            cityName = '北京'
+        }
+        this.props.userInfoActions.update({
+            cityName: cityName
+        })
 
+        // 更改状态
+        this.setState({
+            initDone: true
+        })
     }
 }
-export default App
+
+// -------------------redux react 绑定--------------------
+
+function mapStateToProps(state) {
+    return {
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch),
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
